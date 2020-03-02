157318c68dd05b9fa45f65ff2373c110ae577f45
Garfield Lee <openpgp@550.moe>
2020-03-02 21:19:11 +0800

    feat: add Taro UI

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import Taro, { Component, Config } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
+import { AtButton } from 'taro-ui'
 import './index.scss'
 
 export default class Index extends Component {
@@ -11,6 +12,7 @@ export default class Index extends Component {
     return (
       <View className='index'>
         <Text>Hello world!</Text>
+        <AtButton type='primary'>Taro UI Button</AtButton>
       </View>
     )
   }
