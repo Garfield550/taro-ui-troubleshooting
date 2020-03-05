0c43d2e0c0bf4596d3af1d65a1a4185189f02b68
Garfield Lee <openpgp@550.moe>
2020-03-05 15:04:59 +0800

    feat: add issue 964 solution

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,46 @@
-import Taro, { Component, Config } from '@tarojs/taro'
+import Taro from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
+import { AtTabBar } from 'taro-ui'
 import './index.scss'
 
-export default class Index extends Component {
-  config: Config = {
-    navigationBarTitleText: '首页'
+interface IndexState {
+  current: number
+}
+
+export default class Index extends Taro.Component<{}, IndexState> {
+  public constructor() {
+    super()
+    this.state = {
+      current: 1,
+    }
+  }
+
+  public static config: Taro.Config = {
+    navigationBarTitleText: '首页',
+  }
+
+  private handleClick(current: number) {
+    this.setState({
+      current,
+    })
+    console.log('Current tab:', current)
   }
 
   render() {
     return (
       <View className='index'>
         <Text>Hello world!</Text>
+        <AtTabBar
+          fixed
+          tabList={[
+            { title: '首页', iconType: 'home' },
+            { title: '评论', iconType: 'home' },
+            { title: '引导', iconType: 'home' },
+            { title: '我的', iconType: 'home' },
+          ]}
+          current={this.state.current}
+          onClick={this.handleClick.bind(this)}
+        />
       </View>
     )
   }
