bcfbb8a9a82b35f79255aad91361a189460f176e
Garfield Lee <openpgp@550.moe>
2020-03-13 23:58:40 +0800

    feat: 添加 AtSwitch 示例

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,42 @@
-import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { AtSwitch } from 'taro-ui'
+import { Text, View } from '@tarojs/components'
+import Taro from '@tarojs/taro'
 import './index.scss'
 
-export default class Index extends Component {
-  config: Config = {
+interface IndexStates {
+  checked: boolean
+}
+
+export default class Index extends Taro.Component<{}, IndexStates> {
+  public constructor() {
+    super()
+    this.state = {
+      checked: false
+    }
+  }
+
+  public static config: Taro.Config = {
     navigationBarTitleText: '首页'
   }
 
-  render() {
+  private handleStatusChanged(value: boolean): void {
+    this.setState({
+      checked: value
+    })
+  }
+
+  public render(): JSX.Element {
+    const { checked } = this.state
+
     return (
       <View className='index'>
         <Text>Hello world!</Text>
+        <AtSwitch border={false}
+          color='#006dcc'
+          checked={checked}
+          title={checked ? '状态:在线' : '状态:下线'}
+          onChange={this.handleStatusChanged.bind(this, !checked)}
+        />
       </View>
     )
   }
