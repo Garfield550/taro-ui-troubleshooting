7617713b81fb0077decd10d9091fed2ab24348fb
Garfield Lee <openpgp@550.moe>
2020-03-21 19:05:31 +0800

    chore: add solution

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,21 @@
-import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { Text, View } from '@tarojs/components'
+import Taro from '@tarojs/taro'
+import { AtSwitch, AtInput, AtForm } from 'taro-ui'
 import './index.scss'
 
-export default class Index extends Component {
-  config: Config = {
+export default class Index extends Taro.Component {
+  public static config: Taro.Config = {
     navigationBarTitleText: '首页'
   }
 
-  render() {
+  public render(): JSX.Element {
     return (
       <View className='index'>
         <Text>Hello world!</Text>
+        <AtForm>
+          <AtInput name='input' placeholder='Input' onChange={() => {}} />
+          <AtSwitch title='Switch' />
+        </AtForm>
       </View>
     )
   }
