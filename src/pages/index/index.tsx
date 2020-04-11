7b98300c76ea736695c5198497cdf0be0f125d06
Garfield Lee <openpgp@550.moe>
2020-04-11 19:07:47 +0800

    chore: add test code

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,7 @@
+import { AtSwipeAction } from 'taro-ui'
+import { SwipeActionOption } from 'taro-ui/types/swipe-action'
+import { View, ScrollView } from '@tarojs/components'
 import Taro from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
 import './index.scss'
 
 export default class Index extends Taro.Component {
@@ -7,10 +9,74 @@ export default class Index extends Taro.Component {
     navigationBarTitleText: '首页'
   }
 
+  private handleClick = (item: SwipeActionOption): void => {
+    console.log(item)
+    Taro.showToast({
+      title: `点击了${item.text}`,
+      icon: 'none',
+      duration: 1000
+    })
+  }
+
+  private handleOpened = (): void => {
+    // Taro.showToast({
+    //   title: '打开',
+    //   icon: 'none',
+    //   duration: 1000
+    // })
+  }
+
+  private handleClosed = (): void => {
+    // Taro.showToast({
+    //   title: '关闭',
+    //   icon: 'none',
+    //   duration: 1000
+    // })
+  }
+
   public render(): JSX.Element {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <ScrollView scrollY scrollWithAnimation>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+          <AtSwipeAction autoClose options={[
+            {
+              text: '取消',
+              style: {
+                backgroundColor: '#6190E8'
+              }
+            },
+            {
+              text: '确认',
+              style: {
+                backgroundColor: '#FF4949'
+              }
+            }
+          ]}
+            onClick={this.handleClick}
+            onOpened={this.handleOpened}
+            onClosed={this.handleClosed}
+          >
+            <View className='normal'>点击按钮自动关闭</View>
+          </AtSwipeAction>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+          <View>Hello world!</View>
+        </ScrollView>
       </View>
     )
   }
