f7515dd4f58caf66ffa31fd995e4b3e89f452c22
Garfield Lee <openpgp@550.moe>
2020-04-19 18:22:04 +0800

    chore: add solution

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import Taro from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
+import { AtInput } from 'taro-ui'
 import './index.scss'
 
 export default class Index extends Taro.Component {
@@ -11,6 +12,7 @@ export default class Index extends Taro.Component {
     return (
       <View className='index'>
         <Text>Hello world!</Text>
+        <AtInput name='value' type='password' placeholder='密码' value='000000' onChange={() => {}} />
       </View>
     )
   }
