658def232de991a371f40404eff47f12eb7b7b98
Garfield Lee <openpgp@550.moe>
2020-04-20 23:43:25 +0800

    feat: add solution

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,37 @@
 import Taro from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Picker } from '@tarojs/components'
+import { CommonEvent } from '@tarojs/components/types/common'
+import { AtList, AtListItem } from 'taro-ui'
 import './index.scss'
 
+interface IndexState {
+  dateSel: string
+}
+
 export default class Index extends Taro.Component {
+  public state: IndexState = {
+    dateSel: '2018-04-22'
+  }
+
   public static config: Taro.Config = {
     navigationBarTitleText: '首页'
+  }
+
+  private onDateChange = (e: CommonEvent): void => {
+    this.setState({
+      dateSel: e.detail.value
+    })
   }
 
   public render(): JSX.Element {
     return (
       <View className='index'>
         <Text>Hello world!</Text>
+        <Picker mode='date' value={this.state.dateSel} onChange={this.onDateChange}>
+          <AtList>
+            <AtListItem title='请选择日期' extraText={this.state.dateSel} />
+          </AtList>
+        </Picker>
       </View>
     )
   }
