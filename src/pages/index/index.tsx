13d8dbaac98ccaac645c1462b77dce64d4d561ec
Garfield Lee <openpgp@550.moe>
2020-05-09 14:10:05 +0800

    chore: upgrade Taro to 2.2.3

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import Taro from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
+import { AtButton } from 'taro-ui'
 import './index.scss'
 
 export default class Index extends Taro.Component {
@@ -11,6 +12,7 @@ export default class Index extends Taro.Component {
     return (
       <View className='index'>
         <Text>Hello world!</Text>
+        <AtButton>测试按钮</AtButton>
       </View>
     )
   }
